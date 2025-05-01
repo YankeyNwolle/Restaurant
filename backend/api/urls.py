65b9455ec3748c54f6e27c6b11992cedba647926
yankeynwolle <nwolle14@gmail.com>
2025-05-01 11:21:22 +0000

    Add files via upload

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
-from . import views
 
 from django.urls import path
-from .views import RecetteAPIView
+from .views import RecetteAPIView,OrderAPIView,ProfileAPIView
 
 urlpatterns = [
+    # Routes pour Recette
     path('recettes/', RecetteAPIView.as_view(), name='recettes'),
+
+    # Routes pour Order
+    path('orders/', OrderAPIView.as_view(), name='orders-list'),
+    path('orders/<int:id>/', OrderAPIView.as_view(), name='orders-detail'),
+
+    # Routes pour Profile
+    path('profiles/', ProfileAPIView.as_view(), name='profile-list'),
+    path('profiles/<int:id>/', ProfileAPIView.as_view(), name='profile-detail'),
 ]
 
