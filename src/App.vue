8e3a4dc97cbeee9806d6a76dc75c4f8e8157b52f
yankeynwolle <nwolle14@gmail.com>
2025-05-04 23:40:17 +0000

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <header>
+     
+      <nav>
+        <a href="#">Logo</a>
+        <a href="#">Accueil</a>
+        <a href="#">Recettes</a>
+        <a href="#">Commandes</a>
+      </nav>
+    </header>
+
+    <section class="hero">
+      <h2>Découvrez les meilleurs plats africains !</h2>
+      <p>
+      Plus de 90 000 recettes faciles et rapides pour vous inspirer en cuisine. 
+      </p>
+      
+    </section>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
